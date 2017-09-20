2fad5223f8ac40f533cc6a76133948709c7cec6c
Adam Lovatt <mussyg52@live.co.uk>
2017-09-20 12:04:20 +0100

    Enabled SSL on git

--- production/modules/media/manifests/docker.pp
+++ production/modules/media/manifests/docker.pp
@@ -8,7 +8,7 @@ class media::docker {
     image   => 'linuxserver/plex',
     net     => 'host',
     env     => ['TZ=Europe\London', 'PUID=1001', 'PGID=1001'],
-    volumes => ['/root/docker/plex/config:/config', '/root/md0/TV:/data/tv/shows', '/root/md0/Movies:/data/movies', '/root/docker/plex/transcode:/transcode'],
+    volumes => ['/root/md0/srv/plex/config:/config', '/root/md0/TV:/data/tv/shows', '/root/md0/Movies:/data/movies', '/root/md0/srv/plex/transcode:/transcode'],
 
     }
 
@@ -19,14 +19,8 @@ class media::docker {
 
    docker::run { 'gitlab-ce':
    image    => 'gitlab/gitlab-ce:latest',
-   volumes  => ['/srv/gitlab/config:/etc/gitlab', '/srv/gitlab/logs:/var/log/gitlab', '/srv/gitlab/data:/var/opt/gitlab'],
+   volumes  => ['/root/md0/srv/gitlab/config:/etc/gitlab', '/root/md0/srv/gitlab/logs:/var/log/gitlab', '/root/md0/srv/gitlab/data:/var/opt/gitlab'],
    ports    => ['52443:443', '5222:22', '5280:80'],
    }
-
-   file { "/srv/gitlab/config":
-     ensure  => 'directory',
-     recurse => 'true',
-     source  => 'puppet:///modules/media/docker/gitlab/srv/gitlab/config',
-  }
 
 }
